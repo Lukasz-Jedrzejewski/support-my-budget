da3571fda1b59f7559339c93255c9156e3d7ec29
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-10-08 13:02:20 +0200

    Add dependencies and edit gitignore file

---
+++ src/main/java/com/legion/supportmybudget/SupportMyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.legion.supportmybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportMyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportMyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legion/supportmybudget/SupportMyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legion.supportmybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportMyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
