4d1f68816c61365042e3ac8b47b4998f962397ba
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-10-14 17:11:20 +0200

    Add application.yml file

--- src/main/java/com/legion/supportmybudget/SupportMyBudgetApplication.java
+++ src/main/java/com/legion/supportmybudget/SupportMyBudgetApplication.java
@@ -3,11 +3,12 @@ package com.legion.supportmybudget;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class SupportMyBudgetApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SupportMyBudgetApplication.class, args);
 	}
-
 }
--- src/main/java/com/legion/supportmybudget/test/TestController.java
+++ src/main/java/com/legion/supportmybudget/test/TestController.java
@@ -1,16 +1,23 @@
 package com.legion.supportmybudget.test;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RequestMapping("/")
 @RestController
 public class TestController {
 
-    @GetMapping
-    public ResponseEntity<String> testAction() {
-        return ResponseEntity.ok("Hello!");
+    private final Tess tess;
+
+    public TestController(Tess tess) {
+        this.tess = tess;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> testAction(@RequestParam MultipartFile file) throws IOException {
+        return ResponseEntity.ok(tess.doOcr(file));
     }
 }
