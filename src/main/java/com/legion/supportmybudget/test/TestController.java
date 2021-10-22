b20c25f76099c97615e3e043e60ee36a491324f1
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-10-22 16:07:03 +0200

    Add two endpoints in TestController
    Inject ReceiptMapper into controller above.
    Add action for saving receipt.
    Add action that returns all saved receipts.

--- src/main/java/com/legion/supportmybudget/test/TestController.java
+++ src/main/java/com/legion/supportmybudget/test/TestController.java
@@ -1,26 +1,37 @@
 package com.legion.supportmybudget.test;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RequestMapping("/")
 @RestController
 public class TestController {
 
     private final Tess tess;
+    private final ReceiptMapper mapper;
 
-    public TestController(Tess tess) {
+    public TestController(Tess tess, ReceiptMapper mapper) {
         this.tess = tess;
+        this.mapper = mapper;
     }
 
     @PostMapping
     public ResponseEntity<String> testAction(@RequestParam MultipartFile file) throws IOException {
         return ResponseEntity.ok(tess.doOcr(file));
+    }
+
+    @PostMapping("/db")
+    public ResponseEntity<String> testDb(@RequestBody Receipt receipt) {
+        mapper.insert(receipt);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping
+    public List<Receipt> getAll() {
+        return mapper.findAll();
     }
 }
